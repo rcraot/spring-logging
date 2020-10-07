9a87cae8cbb9d3859c9e8e634d9cba547a88e6d1
Thopucharla <ramchandar.thopucharla@capgemini.com>
2020-10-07 12:14:41 +0530

    Demo on Spring Boot logger configuration

---
+++ src/main/java/com/spring/logging/SpringLoggingApplication.java
@@ -0,0 +1,26 @@
+package com.spring.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoggingApplication implements CommandLineRunner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringLoggingApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLoggingApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        LOGGER.info("This will be printed on info");
+        LOGGER.debug("This will be printed on debug");
+        LOGGER.warn("This will be printed on warn");
+        LOGGER.error("This will be printed on error");
+    }
+}
